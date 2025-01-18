b79bacca7a30128386d562e20c8ff18ddfe48bc6
Kieran Llarena <baymax709@gmail.com>
2025-01-18 15:36:15 -0500

    chore: removed <> wrapper

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -6,34 +6,32 @@ import Link from "next/link";
 
 export default async function Signup() {
   return (
-    <>
-      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?{" "}
-          <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            name="email"
-            placeholder="you@example.com"
-            className="mb-3"
-            required
-          />
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
-          <Button formAction={signUpAction}>Sign up</Button>
-        </div>
-      </form>
-    </>
+    <form className="flex flex-col min-w-64 max-w-64 mx-auto">
+      <h1 className="text-2xl font-medium">Sign up</h1>
+      <p className="text-sm text text-foreground">
+        Already have an account?{" "}
+        <Link className="text-primary font-medium underline" href="/sign-in">
+          Sign in
+        </Link>
+      </p>
+      <div className="flex flex-col gap-2 mt-8">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          name="email"
+          placeholder="you@example.com"
+          className="mb-3"
+          required
+        />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          placeholder="Your password"
+          minLength={6}
+          required
+        />
+        <Button formAction={signUpAction}>Sign up</Button>
+      </div>
+    </form>
   );
 }
