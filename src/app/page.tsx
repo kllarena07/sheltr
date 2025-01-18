8abae90a46df880fe2c8727cb1ccd7e363e9720b
Kieran Llarena <baymax709@gmail.com>
2025-01-18 15:01:27 -0500

    feat: supabase sign up + login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,44 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { signInAction } from "./actions";
 
-export default function Home() {
+export default function SignIn() {
   return (
     <section className="h-dvh flex flex-col justify-center p-5">
       <h1 className="text-3xl text-center font-bold">Welcome to Sheltr</h1>
       <p className="text-center text-gray-600">
         Crowdsourced disaster overwatch
       </p>
-      <section className="flex flex-col my-5 gap-5">
+      <form className="flex flex-col my-5 gap-5">
         <Label className="flex flex-col gap-3">
           <span className="font-bold t">Email</span>
           <Input
+            name="email"
             placeholder="Enter your email"
             type="text"
             className="py-6"
+            required
           ></Input>
         </Label>
         <Label className="flex flex-col gap-3">
           <span className="font-bold">Password</span>
           <Input
+            name="password"
             placeholder="Enter your password"
             type="password"
             className="py-6"
+            required
           ></Input>
         </Label>
-        <Button className="w-full font-bold h-[50px]">Login</Button>
-      </section>
+        <Button
+          className="w-full font-bold h-[50px]"
+          type="submit"
+          formAction={signInAction}
+        >
+          Login
+        </Button>
+      </form>
       <section className="flex justify-between">
         <Link href="/forgot-password" className="font-bold text-blue-600">
           Forgot Password?
