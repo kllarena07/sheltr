0a4c6ed3286f836323ac76e907b3ab579dc14489
Kieran Llarena <baymax709@gmail.com>
2025-01-18 12:51:36 -0500

    refactor: initial outline for feed page

--- src/app/protected/feed/page.tsx
+++ src/app/protected/feed/page.tsx
@@ -1,3 +1,10 @@
 export default function Feed() {
-  return <section>This is the feed page</section>;
+  return (
+    <section className="px-2 py-7">
+      <h1 className="text-black text-center text-3xl font-bold">
+        Sheltr Event Feed
+      </h1>
+      {/* PUT EVENTS HERE */}
+    </section>
+  );
 }
