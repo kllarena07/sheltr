8ce3b7f8f7935935a489a2e225cbefda0158ef6c
Kieran Llarena <baymax709@gmail.com>
2025-01-18 16:13:13 -0500

    feat: mapbox map view

--- src/app/protected/map/page.tsx
+++ src/app/protected/map/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { Map as MapView } from "react-map-gl";
+
 export default function Map() {
-  return <section>This is the map page</section>;
+  return (
+    <div className="max-w-full h-full overflow-hidden">
+      <MapView
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        attributionControl={false}
+      />
+    </div>
+  );
 }
