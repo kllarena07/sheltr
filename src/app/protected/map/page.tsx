70e44334181d86d450c59334eae40567d18dfa20
Kieran Llarena <baymax709@gmail.com>
2025-01-18 22:53:45 -0500

    feat: live updating map

--- src/app/protected/map/page.tsx
+++ src/app/protected/map/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Map as MapView, Marker } from "react-map-gl";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Select } from "@radix-ui/react-select";
 import {
@@ -13,6 +13,7 @@ import {
 import { Button } from "@/components/ui/button";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { createReportAction } from "@/app/actions";
+import { createClient } from "@/utils/supabase/client";
 
 export default function Map() {
   const [createState, setCreateState] = useState(false);
@@ -20,6 +21,71 @@ export default function Map() {
     longitude: -122.4,
     latitude: 37.8,
   });
+
+  const [disasters, setDisasters] = useState([]);
+
+  useEffect(() => {
+    const supabase = createClient();
+
+    const channel = supabase
+      .channel("disasters_changes")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "disasters" },
+        (payload) => {
+          if (payload.eventType === "INSERT") {
+            setDisasters((prev) => [...prev, payload.new]);
+          } else if (payload.eventType === "UPDATE") {
+            setDisasters((prev) =>
+              prev.map((disaster) =>
+                disaster.id === payload.new.id ? payload.new : disaster
+              )
+            );
+          } else if (payload.eventType === "DELETE") {
+            setDisasters((prev) =>
+              prev.filter((disaster) => disaster.id !== payload.old.id)
+            );
+          }
+          console.log("Change received!", disasters);
+        }
+      )
+      .subscribe();
+
+    const fetchDisasters = async () => {
+      const { data, error } = await supabase.from("disasters").select("*");
+      if (data) {
+        const formattedData = await Promise.all(
+          data.map(async (disaster) => {
+            const { created_at, user_id, location, ...rest } = disaster;
+
+            const date = new Date(created_at);
+            const formattedDate = date.toLocaleDateString();
+            const formattedTime = date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            });
+            const formattedLocation = JSON.parse(location);
+
+            return {
+              ...rest,
+              location: formattedLocation,
+              created_at: `${formattedDate}, ${formattedTime}`,
+            };
+          })
+        );
+
+        console.log(formattedData);
+        setDisasters(formattedData);
+      }
+      if (error) console.error("Error fetching disasters:", error);
+    };
+    fetchDisasters();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
 
   return (
     <section className="relative max-w-full h-full overflow-hidden">
@@ -39,12 +105,15 @@ export default function Map() {
           mapStyle="mapbox://styles/mapbox/streets-v9"
           attributionControl={false}
         >
-          <Marker
-            longitude={markerPosition.longitude}
-            latitude={markerPosition.latitude}
-            anchor="bottom"
-            offset={[0, 0]}
-          />
+          {disasters.map(({ id, location }) => (
+            <Marker
+              key={id}
+              longitude={location[0]}
+              latitude={location[1]}
+              anchor="bottom"
+              offset={[0, 0]}
+            />
+          ))}
         </MapView>
         {createState ? (
           <div
